f61b81821f141dae61e02c2b96a5a31486b0cfa7
Frisky211 <70955624+Frisky211@users.noreply.github.com>
2021-12-16 17:42:59 +0300

    Add files via upload

---
+++ lab5/lab5_1.py
@@ -0,0 +1,20 @@
+import MySQLdb
+
+db = MySQLdb.connect(
+    host='localhost',
+    user='dbuser',
+    passwd='03_VlAdIk_2001',
+    db='first_db'
+)
+
+c = db.cursor()
+c.execute("INSERT INTO books (name, dicription) VALUES (%s, %s);", ('Книга', 'Описание'))
+db.commit()
+c.execute("SELECT * FROM books")
+records = c.fetchall()
+for row in records:
+    print(row)
+c.execute("DELETE FROM BOOKS")
+db.commit()
+c.close()
+db.close()
